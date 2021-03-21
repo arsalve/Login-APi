39015e745c0a0fc73529d26e0d9fa8cb394af602
Ashish Salve <a.r.salve@gmail.com>
2021-03-21 13:35:21 +0530

    publish

--- user.js
+++ user.js
@@ -4,27 +4,7 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 const schema = mongoose.Schema({
-    'Email': {
-        type: String,
-        unique: true,
-        validate:{
-            validator: emailRegexp.test,
-            message: '{VALUE} is not a valid email',
-            isAsync: false
-          }
-    },
-    'firstName': {
-        type: String
-    },
-    'lastName': {
-        type: String
-    },
-    'address': {
-        type: String
-    }, 
-    'createdBy': {
-        type: String
-    }, 
+  
     'fullName': {
         type: String,
         default: function(){return this.firstName+" "+this.lastName}
