5296cdd9529112c3a7a6a299bafe93810e0ab82a
Ashish R Salve <a.r.salve@gmail.com>
2021-08-01 19:23:22 +0530

    added Word game

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cros=require('cors')
 const suc = chalk.greenBright;
 const port = process.env.PORT || 8080;
 const UserRouter = require('./Router.js');
+const path = require('path');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -13,9 +14,13 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cros());
 app.use(UserRouter)
-
+app.use(express.static(path.join(__dirname, './Public')));
 app.get('/', (req, res) => {
-  res.status(200).send("Server is running")
+  fs.readFile(__dirname + '\\index.html', 'utf8', function (err, text) {
+    res.send(text, {
+        root: './'
+    });
+});
 })
 
 app.listen(port, () => {
